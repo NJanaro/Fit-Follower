8633bb102a87991818a4e3acba482a6f542d43b7
Nick Janaro <nick.janaro@gmail.com>
2020-09-21 18:05:59 -0400

    Getting a show up

---
+++ app/frontend/components/component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Component extends React.Component{
+
+    render(){
+        return (
+            <h1>Look at me! i'm a fit follower!</h1>
+        )
+    }
+}
--- app/frontend/fit_follower_index.jsx
+++ app/frontend/fit_follower_index.jsx
@@ -1,3 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+document.addEventListener("DOMContentLoaded", ()=>{
+    const root = document.getElementById("root");
+    ReactDOM.render(<Component/>, root);
+})
