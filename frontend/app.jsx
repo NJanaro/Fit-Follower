d09cb3aa4673c3426b1787ff5bcceee6746a4692
nick <nick.janaro@gmail.com>
2020-11-23 15:52:24 -0500

    Fixed routing so browser navigation buttons work as they should

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
+import NavLoginContainer from './components/navbar/nav_login_container';
 import SignupUserContainer from './components/welcome/create_user_container';
 import LoginUserContainer from './components/welcome/login_user_container';
 import NavSignupContainer from './components/navbar/nav_signup_container';
-import NavLoginContainer from './components/navbar/nav_login_container';
-// import NavDashboardContainer from './components/navbar/nav_dashboard_container'; this will go in once a user has been logged in
-import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
+import { Route, Redirect, Switch, Link, HashRouter, withRouter } from "react-router-dom";
 import { AuthRoute, ProtectedRoute } from './utils/front_end_route_utils';
 import Splash from './components/splash/splash';
 import Home from './components/home'
@@ -14,11 +13,11 @@ import Home from './components/home'
 class App extends React.Component {
 
     render(){
+        const NavBar = withRouter(NavLoginContainer)
         return(
             <>
-            <Route path='/' component={NavLoginContainer}/>
+            <NavBar/>
             <Switch>
-                {/* protected rout /home that will render the 3 protected routes on 23-25 */}
                 <ProtectedRoute path='/home' component={Home}/> 
                 <AuthRoute exact path='/login' component={LoginUserContainer}/>
                 <AuthRoute exact path='/signup' component={SignupUserContainer}/>
