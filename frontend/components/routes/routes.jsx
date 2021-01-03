f3b4235a935e4a4bdac8f8a184bc844aee20339f
nick <nick.janaro@gmail.com>
2021-01-03 16:22:36 -0500

    added info to routes page and styled

--- frontend/components/routes/routes.jsx
+++ frontend/components/routes/routes.jsx
@@ -25,8 +25,10 @@ class Routes extends React.Component {
       return (
         <>
         <div className="myMiniMaps">
-          {Object.values(this.props.routes).map((route, idx)=> (
-            <div key={`route=${idx}`} className="miniMap">
+          {Object.values(this.props.routes).map((route, idx)=> {
+            let time = route.created_at.split("T")
+            // debugger
+            return <div key={`route=${idx}`} className="miniMap">
               {/* <MiniMap info={route}/> */}
               <Link to={{
                           pathname: '/home/routes/edit',
@@ -39,11 +41,36 @@ class Routes extends React.Component {
                           <MiniMap info={route}/>
                          </span>
               </Link>
-              <div>{route.route_name}</div>
+              <div className="routeInfo">
+              <Link className="routeName" to={{
+                          pathname: '/home/routes/edit',
+                          state: {
+                            info: route
+                          }
+                        }
+                       }>
+                      {route.route_name}
+              </Link>
+
+              {/* created_at: "2020-12-30T18:52:32.530Z"
+              description: "Walking"
+              distance: "21.88 Miles"
+              id: 3
+              route_info: "{"origin":{"lat":40.814877738664926,"lng":-73.99315749230956},"destination":{"lat":40.845876735943875,"lng":-73.89702712121581},"waypoints":[{"location":{"lat":40.814877738664926,"lng":-73.99315749230956}},{"location":{"lat":40.74871668464755,"lng":-73.918313131958}},{"location":{"lat":40.82604971858595,"lng":-73.89410887780761}},{"location":{"lat":40.836930920074984,"lng":-73.92717361450195}},{"location":{"lat":40.845876735943875,"lng":-73.89702712121581}}],"travelMode":"WALKING"}"
+              route_name: "Walk"
+              user_id: 1 */}
+
+                <div className="routeInfo-spec">{route.distance}<br /> <p className="type">Distance</p> </div>
+                <div className="routeInfo-spec">{route.description}<br /> <p className="type">description</p> </div>
+                <div className="routeInfo-time">Created on {time[0]}</div>
+
+
+              </div>
               {/* </div> */}
             </div>
-          ))}
+  })}
         </div>
+      
         </>
       )   
   }
