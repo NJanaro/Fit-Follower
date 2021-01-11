a80cf9f567e8a9302eb0b429664a93223cc88a4a
najanaro <nick.janaro@gmail.com>
2021-01-11 16:40:52 -0500

    Continued work on set

--- frontend/components/workouts/workout_form.jsx
+++ frontend/components/workouts/workout_form.jsx
@@ -15,14 +15,20 @@ class WorkoutForm extends React.Component {
               <div className="my-routes-top">
                 <h1 id="page-title">{this.props.newOrEdit}</h1>
               </div>      
-                <div className="workout-form">
-                    <form>
+                <div>
+                    <form className="workout-form">
                         <div className="workoutDiv">
                             
                             <label htmlFor="distance">Distance</label>
-                            <input id="distance" className="workout-input" type="text"/>
-                            <label htmlFor="sport">Sport</label>
-                            <input id="sport" className="workout-input" type="text"/>
+                            <input style={{width:"50px"}} id="distance" className="workout-input" type="number"/>
+                            <label htmlFor="duration">Duration</label>
+                            <input style={{width:"30px", marginRight:"0", borderRadius:"0"}} id="duration" className="workout-input" type="number"/>
+                            <input style={{width:"30px", marginLeft:"0", borderRadius:"0"}} id="duration" className="workout-input" type="number"/>
+                            <label htmlFor="sport" style={{marginRight:"8px"}}>Sport</label>
+                            <select id="sport">
+                                <option value="WALKING">Run</option>
+                                <option value="BICYCLING">Bike</option>
+                            </select>
                         </div>
                         <div className="workoutDiv">
                             <label htmlFor="workoutName">Workout Name</label>
@@ -30,11 +36,11 @@ class WorkoutForm extends React.Component {
                             <label htmlFor="workoutDescription">Description</label>
                             <input id="workoutDescription" className="workout-input" type="text"/>
                             <label htmlFor="dateComplete">Date Finished</label>
-                            <input id="dateComplete" className="workout-input" type="text"/>
+                            <input style={{width:"200px "}} id="dateComplete" className="workout-input" type="date"/>
                         </div>
                         <div className="workoutDiv">
-                            <input id="duration" className="workout-input" type="text"/>
-                            <input id="averagePace" className="workout-input" type="text"/>
+                            <label htmlFor="averagePace">Pace</label>
+                            <input style={{width:"50px"}} id="averagePace" className="workout-input" type="text" disabled/>
                         </div>
                     </form>
                 </div>
