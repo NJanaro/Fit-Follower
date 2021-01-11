a69d3d0d928936c5372f2e31e9a0723e97d229ab
najanaro <nick.janaro@gmail.com>
2021-01-11 14:23:15 -0500

    added labels to inputs

--- frontend/components/workouts/workout_form.jsx
+++ frontend/components/workouts/workout_form.jsx
@@ -18,12 +18,18 @@ class WorkoutForm extends React.Component {
                 <div className="workout-form">
                     <form>
                         <div className="workoutDiv">
+                            
+                            <label htmlFor="distance">Distance</label>
                             <input id="distance" className="workout-input" type="text"/>
+                            <label htmlFor="sport">Sport</label>
                             <input id="sport" className="workout-input" type="text"/>
                         </div>
                         <div className="workoutDiv">
+                            <label htmlFor="workoutName">Workout Name</label>
                             <input id="workoutName" className="workout-input" type="text"/>
+                            <label htmlFor="workoutDescription">Description</label>
                             <input id="workoutDescription" className="workout-input" type="text"/>
+                            <label htmlFor="dateComplete">Date Finished</label>
                             <input id="dateComplete" className="workout-input" type="text"/>
                         </div>
                         <div className="workoutDiv">
